f80a29eb8d4d99c989dcfbc6d5faeb7a7b3a5891
zhangmin <512444693@qq.com>
2016-06-21 20:51:26 +0800

    改名

--- src/test/java/com/zm/PersonalAssistant/Reminder/ReminderTest.java
+++ src/test/java/com/zm/PersonalAssistant/Reminder/ReminderTest.java
@@ -23,3 +23,15 @@ public class ReminderTest {
         assertEquals(2, reminder.getAdvancedNotifyList().size());
     }
 }
+
+//TODO : 提前通知测试，单个提前，多个一起提前等
+
+//TODO : 正式通知测试
+
+//TODO : 提前几 分钟、小时、天、月 通知测试
+
+//TODO : 永不，每天，每周 ，每月， 每年 重复测试
+
+//TODO : 添加，删除，更新（得到通知） Reminder 列表顺序测试
+
+//TODO : 得到几天内的Reminder列表
